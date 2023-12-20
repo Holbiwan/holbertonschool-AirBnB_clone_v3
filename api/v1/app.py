b0545fe455de87296239f194ae8f3e51ee01dfdb
powerofcode2023 <holbertonc21@gmail.com>
2023-12-20 15:24:29 +0100

    fixed pycocestyle

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,10 +11,12 @@ from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """ Called to close the storage after each request"""
     storage.close()
+
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
