f3b416c69ae20710f65f7d53c9826faaf7eb3d3e
Holbiwan <sabrinapapeau@gmail.com>
2023-12-20 16:38:19 +0100

    create endpoint retrieving number of each number

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,30 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """Returns a JSON response with the status of the API"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """Returns a JSON response with the status of the API"""
+    return jsonify({"status": "OK"})
+
+
+@app_views.route('/api/v1/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """Retrieves the number of each object type"""
+    stats_dict = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+    }
+    return jsonify(stats_dict)
