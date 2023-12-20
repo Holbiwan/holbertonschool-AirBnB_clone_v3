53cfe3a3deee2ba7458d6017e4cdfeb4276df281
powerofcode2023 <holbertonc21@gmail.com>
2023-12-20 15:22:14 +0100

    reintroduce the files to the projet

---
+++ api/v1/app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Initializes the Flask application"""
+
+from flask import Flask
+from flask import Blueprint
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """ Called to close the storage after each request"""
+    storage.close()
+
+if __name__ == "__main__":
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = os.getenv('HBNB_API_PORT', '5000')
+    app.run(host=host, port=port, threaded=True)
