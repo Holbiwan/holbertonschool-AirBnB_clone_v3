d18f1b1745031d921fc0957f1b80dc4a83796c59
powerofcode2023 <holbertonc21@gmail.com>
2023-12-20 15:25:04 +0100

    fixed pycocestyle

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@
 from api.v1.views import app_views
 from flask import jsonify
 
+
 @app_views.route('/status', methods=['GET'])
 def status():
     """Returns a JSON response with the status of the API"""
