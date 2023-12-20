1ccb7fdc5d9c60bfb562bec79f5d222f82bd5034
Holbiwan <sabrinapapeau@gmail.com>
2023-12-20 18:27:22 +0100

    create handler for 404 errors returns a JSON-formatted status

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python3
 """Initializes the Flask application"""
 
-from flask import Flask
-from flask import Blueprint
 from models import storage
 from api.v1.views import app_views
-import os
-
+import os import getenv
+from flask import Flask, jsonify, Blueprint
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """ Called to close the storage after each request"""
+    """Called to close the storage after each request"""
     storage.close()
+
+
+@app.errorhandler(404)
+def handler_error(stat):
+    """returns a JSON-formatted 404 status code response"""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
